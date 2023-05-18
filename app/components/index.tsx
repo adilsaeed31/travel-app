14d9314ded85a640c93a7490ab5c6e407de0a8e3
Piyush Kapoor <pkapoor@saib.com.sa>
2023-05-18 09:53:13 +0300

    adding ts instead of tsx

--- app/components/index.tsx
+++
@@ -1,7 +0,0 @@
-export {default as AppContextButtons} from './AppContextButtons'
-export {default as TCTextView} from './TextView'
-export {default as TCListItem} from './ListItem'
-export {default as TCButton} from './Button'
-export {default as TCInput} from './Input'
-export {default as Layout} from './Layout'
-export {default as Spacer} from './Spacer'
