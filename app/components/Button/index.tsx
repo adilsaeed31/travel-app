fa6f9fa809acfd412ec51d2034f272ddd288cbf0
Piyush Kapoor <pkapoor@saib.com.sa>
2023-05-18 15:11:43 +0300

    feat(Button): added ripple

--- app/components/Button/index.tsx
+++ app/components/Button/index.tsx
@@ -1,25 +1,43 @@
 import React from 'react'
-import {Button} from '@ui-kitten/components'
+import {View} from 'react-native'
 import {styled} from 'styled-components/native'
 import {useStore} from '@Store'
+import Ripple, {RippleProps} from 'react-native-material-ripple'
 
-const StyledButton = styled(Button)<{isRTL: boolean}>`
-  text-align: ${({isRTL}) => (isRTL ? 'right' : 'left')};
+const StyledRipple = styled(Ripple)<{isRTL: boolean}>`
+  align-self: ${({isRTL}) => (isRTL ? 'flex-end' : 'flex-start')};
 `
 
-interface ButtonProps {
+const StyledView = styled(View)<{isRTL: boolean}>`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`
+
+interface ButtonProps extends RippleProps {
   children: React.ReactElement
+  RippleColor?: string
   onPress?: () => void
   className?: string
 }
 
-const TCButton: React.FC<ButtonProps> = ({children, onPress, ...rest}) => {
+const TCButton: React.FC<ButtonProps> = ({
+  children,
+  onPress,
+  RippleColor = 'white',
+  ...rest
+}) => {
   const isRTL = useStore.getState().isRTL
 
   return (
-    <StyledButton onPress={onPress} isRTL={isRTL} {...rest}>
-      {children}
-    </StyledButton>
+    <StyledRipple
+      onPress={onPress}
+      isRTL={isRTL}
+      rippleColor={RippleColor}
+      {...rest}>
+      <StyledView isRTL={false}>{children}</StyledView>
+    </StyledRipple>
   )
 }
 
