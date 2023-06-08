56700da490fbaa7eca3969253bfc9000a303e851
abdul jaleel <ajaleel@saib.com.sa>
2023-06-08 14:28:41 +0300

    translation cache fixed

--- App.tsx
+++ App.tsx
@@ -8,13 +8,39 @@
 import React from 'react'
 import {LogBox} from 'react-native'
 import {QueryClientProvider} from '@tanstack/react-query'
+import {translation} from '@Utils'
 
 import {queryClient} from '@Api'
+import i18next from 'i18next'
+import {initReactI18next} from 'react-i18next'
 
 import {MainNavigation} from '@Navigations'
 import {AuthProvider, AppProvider} from '@Context'
+import {useStore} from '@Store'
+interface CustomLanguageDetectorType {
+  type: 'languageDetector'
+  async: Boolean
+  detect: (cb: (lng: string) => void) => void
+  init: (options: any) => void
+  cacheUserLanguage: (lng: string) => void
+}
 
 function App(): React.JSX.Element {
+  const languageDetector: CustomLanguageDetectorType = {
+    type: 'languageDetector',
+    async: true,
+    detect: (cb: any) => cb(useStore.getState().language),
+    init: () => {},
+    cacheUserLanguage: () => {},
+  }
+
+  i18next.use(languageDetector).use(initReactI18next).init({
+    fallbackLng: 'en',
+    compatibilityJSON: 'v3',
+    debug: false,
+    resources: translation,
+  })
+
   LogBox.ignoreAllLogs()
   return (
     <QueryClientProvider client={queryClient}>
