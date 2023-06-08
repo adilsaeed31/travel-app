0288ac2e37fd95b95e1b165bfaf928aa418195ba
abdul jaleel <ajaleel@saib.com.sa>
2023-06-08 17:38:54 +0300

    listviewitemadded and account screen modified

---
+++ app/features/Dashboard/Account/ListViewItem.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import styled from 'styled-components/native'
+import {vh, vw} from '@Utils'
+
+const ListItemContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 10px;
+  width: 100%;
+`
+
+const Circle = styled.View`
+  width: ${40}px;
+  height: ${40}px;
+  border-radius: ${20}px;
+  background-color: #f5f5f5;
+  align-items: center;
+  justify-content: center;
+`
+
+const CircleContainer = styled.View`
+  flex: 1;
+  flex-grow: 0.3;
+  align-items: center;
+`
+const TitleContainer = styled.View`
+  flex: 1;
+`
+
+const Title = styled.Text`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 18px;
+`
+
+const Subtitle = styled.Text`
+  font-size: 14px;
+  color: #888888;
+`
+
+const Number = styled.Text`
+  font-style: normal;
+  flex: 1;
+  flex-grow: 0.5;
+  text-align: right;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 14px;
+`
+
+const ListViewItem = ({icon, title, subtitle, number}) => {
+  return (
+    <ListItemContainer>
+      <CircleContainer>
+        <Circle>
+          <Subtitle>CA</Subtitle>
+        </Circle>
+      </CircleContainer>
+      <TitleContainer>
+        <Title>{title}</Title>
+        <Subtitle>{subtitle}</Subtitle>
+      </TitleContainer>
+      <Number>{number}</Number>
+    </ListItemContainer>
+  )
+}
+
+export default ListViewItem
--- app/features/Dashboard/Account/index.tsx
+++ app/features/Dashboard/Account/index.tsx
@@ -1,23 +1,46 @@
 import React, {memo} from 'react'
-import {ScrollView} from 'react-native'
+import {ScrollView, View} from 'react-native'
 import Animated, {FadeInRight} from 'react-native-reanimated'
 
-import {QuickActions, UserAccountCardView} from '@Components'
+import {Layout, QuickActions, UserAccountCardView} from '@Components'
+import ListViewItem from './ListViewItem'
+import {FaceIcon} from '@Assets'
 
-type TravelCardScreenProps = {
+type AccountScreenScreenProps = {
   data?: object
 }
 
-const TravelCardScreen: React.FC<TravelCardScreenProps> = () => {
+const AccountScreen: React.FC<AccountScreenScreenProps> = () => {
   return (
-    <ScrollView>
-      <Animated.View entering={FadeInRight.duration(1000).delay(50)}>
+    <ScrollView style={{height: '100%', width: '100%'}}>
+      <View style={{flex: 3}}>
         <UserAccountCardView />
-      </Animated.View>
-
-      <QuickActions />
+      </View>
+      <View style={{flex: 1, flexGrow: 0.8}}>
+        <QuickActions />
+      </View>
+      <View style={{flex: 2, marginBottom: 60}}>
+        <ListViewItem
+          icon={<FaceIcon />}
+          title={'Card Fees *2395'}
+          subtitle={'12 Feb, 13:00'}
+          number={'-100000'}
+        />
+        <ListViewItem
+          icon={<FaceIcon />}
+          title={'Card debit *3395'}
+          subtitle={'12 Feb, 13:00'}
+          number={'-100000'}
+        />
+        <ListViewItem
+          icon={<FaceIcon />}
+          title={'Card credit *3398'}
+          subtitle={'12 Feb, 13:00'}
+          number={'12000'}
+        />
+      </View>
     </ScrollView>
   )
 }
 
-export default memo(TravelCardScreen)
+export default memo(AccountScreen)
