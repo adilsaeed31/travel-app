9ebc2659770005d73c3a842a1bb6cc65a435ead5
Muhammad Adil <w157mumo@saib.com.sa>
2023-06-12 10:23:43 +0300

    add stylesheet for currencyRow

--- app/components/Dashboard/TravelCard/CurrencyRow/index.tsx
+++ app/components/Dashboard/TravelCard/CurrencyRow/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Image, View, TouchableOpacity} from 'react-native'
+import {Image, View, TouchableOpacity, StyleSheet} from 'react-native'
 import {useTranslation} from 'react-i18next'
 import cn from 'classnames'
 
@@ -12,9 +12,6 @@ const CurrencyRow = () => {
   const {t} = useTranslation()
   const isRTL = useStore(state => state.isRTL)
 
-  const clsML: string = ml(isRTL, 2)
-  const clsML1: string = ml(isRTL, 1)
-
   return (
     <View
       className={cn(
@@ -23,8 +20,8 @@ const CurrencyRow = () => {
       )}>
       <View className={cn(flexRowLayout(isRTL), 'items-center')}>
         <Image source={EuroFlag} />
-        <TCTextView className={clsML}>212</TCTextView>
-        <TCTextView className={cn(clsML1, 'font-tc-light')}>
+        <TCTextView style={styles.leftSpace}>212</TCTextView>
+        <TCTextView className="font-tc-light">
           {t('TravelCard:currentCodeEuro', {amount: '.34'})}
         </TCTextView>
       </View>
@@ -35,5 +32,11 @@ const CurrencyRow = () => {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  leftSpace: {
+    marginStart: 4,
+  },
+})
 
 export default CurrencyRow
